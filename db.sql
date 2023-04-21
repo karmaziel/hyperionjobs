426356a73e16261de40bf41bfc4179e97a70b391
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2023-04-21 08:23:11 -0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,7 +1,13 @@
+--CREATE DATABASE HYPERIONJOBS
+--USE HYPERIONJOBS
+
 CREATE TABLE users (
-  id INT(11) AUTO_INCREMENT PRIMARY KEY,
+  id_user INT(11) AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  nombre VARCHAR(255) NOT NULL,
+  apellido VARCHAR(255) NOT NULL,
+  correo VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE empresas (
@@ -20,5 +26,23 @@ CREATE TABLE trabajos (
   sueldo MONEY NOT NULL,
   fecha_limite DATE NOT NULL,
   ubicacion VARCHAR(225) NOT NULL
+);
 
+CREATE TABLE proy4ectos (
+  id_proyecto VARCHAR(10) PRIMARY KEY,
+  categoria VARCHAR(50) NOT NULL,
+  name_proyecto VARCHAR(50) NOT NULL,
+  descripcion VARCHAR(225) NOT NULL,
+  fecha_limite DATE NOT NULL,
+  ubicacion VARCHAR(225) NOT NULL
+);
+
+CREATE TABLE solicitud_trabajo (
+  id_user INT(11),
+  id_Trabajo VARCHAR(10) NOT NULL,
+  nombre VARCHAR(255) NOT NULL,
+  apellido VARCHAR(255) NOT NULL,
+  correo VARCHAR(255) NOT NULL,
+  portafolio VARCHAR(255) NOT NULL,
+  comentario VARCHAR(255) NOT NULL
 );
