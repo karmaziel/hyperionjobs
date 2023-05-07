ac1a6987b73a10709a3fcb4c57a33ed6a14d6f3d
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2023-05-06 18:20:31 -0700

    Update apply-job [WIP]

--- js/search.js
+++ js/search.js
@@ -1,19 +1,10 @@
-const Job = require("../model/Job");
+const joblist = require("../views/job-list");
 
-var key;
-
-const jobsinfo=Job.find().toArray();
 
 function search(data)
 {
-    key=data;
-    jobsinfo.forEach(job => {
-        if(key==job.title)
-        {
-            return
-            <h3 class="mb-3">job.title</h3>,
-            <span class="text-truncate me-3"><i class="fa fa-map-marker-alt text-primary me-2"></i>job.location</span>,
-            <span class="text-truncate me-0"><i class="far fa-money-bill-alt text-primary me-2"></i>job.salary</span>
-        }
-    });
+    key=title;
+    var job_title=document.getElementById("title");
+    var job_salary=document.getElementById("salary");
+    var job_location=document.getElementById("location");
 }
